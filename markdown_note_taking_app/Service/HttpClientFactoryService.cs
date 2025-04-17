057801c16b327fcab5060b67025eeb0fa2d32efb
PaulAllan209 <dpaulallan14@gmail.com>
2025-04-17 15:14:11 +0800

    deleted unimportant codes in HttpClientFactory

--- markdown_note_taking_app/Service/HttpClientFactoryService.cs
+++ markdown_note_taking_app/Service/HttpClientFactoryService.cs
@@ -17,32 +17,16 @@ namespace markdown_note_taking_app.Service
 
         public async Task<string> MakeHttpRequestFromLanguageToolApi(string content)
         {
-            var payload = new
-            {
-                text = content,
-                language = "en-US"
-            };
-
-
             var formContent = new FormUrlEncodedContent(new Dictionary<string, string>
             {
                 { "text", content },
                 { "language", "en-US" }
             });
-           
 
             var httpClient = _httpClientFactory.CreateClient();
-            var jsonContent = JsonContent.Create(payload);
-
-            var jsonSerialized = JsonSerializer.Serialize(payload);
-            _logger.LogInfo($"Serialized JSON payload: {payload}");
 
             using (var response = await httpClient.PostAsync("https://api.languagetool.org/v2/check", formContent))
             {
-                _logger.LogInfo($"Response Headers: {response.Headers}");
-                _logger.LogInfo($"Response Content-Type: {response.Content.Headers.ContentType}");
-                _logger.LogInfo("Line before EnsureSuccessStatusCode");
-                _logger.LogInfo($"The response content : {await response.Content.ReadAsStringAsync()}");
                 response.EnsureSuccessStatusCode();
                 return await response.Content.ReadAsStringAsync();
             }
