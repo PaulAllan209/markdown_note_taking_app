6db8f4f0a090ea0b9d62ef63d5ea75979178cc35
PaulAllan209 <dpaulallan14@gmail.com>
2025-05-03 20:21:34 +0800

    added file input window for uploading .md files

--- markdown_note_taking_app.client/src/SideBar.jsx
+++ markdown_note_taking_app.client/src/SideBar.jsx
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './SideBar.css'
 function SideBar() {
     const [fileNames, setFileNames] = useState([]);
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [uploadFile, setUploadFile] = useState(null);
+    const fileInputRef = useRef(null);
 
     useEffect(() => {
         fetch('https://localhost:7271/api/markdown')
@@ -15,12 +18,35 @@ function SideBar() {
             })
     }, []);
 
+    const handleFileUpload = (event) => {
+        const file = event.target.files[0];
+        if (file && file.type === "text/markdown") {
+            setSelectedFile(file);
+        }
+        else {
+            alert("Please select a valid .md file.");
+        }
+    }
+
+
+    const triggerFileInput = () => {
+        fileInputRef.current.click();
+    };
+
 
 
     return (
       <div className="side-bar">
             <button className="side-bar-buttons"><img src="/assets/button_icons/add_file.png" className="side-bar-icons"></img></button>
-            <button className="side-bar-buttons"><img src="/assets/button_icons/upload_file.png" className="side-bar-icons"></img></button>
+            
+            <button className="side-bar-buttons" onClick={triggerFileInput}><img src="/assets/button_icons/upload_file.png" className="side-bar-icons"></img></button>
+            <input
+                type="file"
+                accept=".md"
+                ref={fileInputRef}
+                style={{ display: 'none' }}
+                onChange={handleFileUpload}
+            />
             <button className="side-bar-buttons"><img src="/assets/button_icons/delete_file.png" className="side-bar-icons"></img></button>
             <button className="side-bar-buttons"><img src="/assets/button_icons/edit_file.png" className="side-bar-icons"></img></button>
 
