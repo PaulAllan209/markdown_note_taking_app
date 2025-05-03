e9139bc9259e78a9fadb1d6e1b1a1d6cb6c1028e
PaulAllan209 <dpaulallan14@gmail.com>
2025-05-03 15:21:38 +0800

    designed the sidebar buttons

--- markdown_note_taking_app.client/src/SideBar.jsx
+++ markdown_note_taking_app.client/src/SideBar.jsx
@@ -20,6 +20,7 @@ function SideBar() {
     return (
       <div className="side-bar">
             <button className="side-bar-buttons"><img src="/assets/button_icons/add_file.png" className="side-bar-icons"></img></button>
+            <button className="side-bar-buttons"><img src="/assets/button_icons/upload_file.png" className="side-bar-icons"></img></button>
             <button className="side-bar-buttons"><img src="/assets/button_icons/delete_file.png" className="side-bar-icons"></img></button>
             <button className="side-bar-buttons"><img src="/assets/button_icons/edit_file.png" className="side-bar-icons"></img></button>
 
