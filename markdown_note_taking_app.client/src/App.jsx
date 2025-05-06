2fd6c0ebd49239cd39b5e240393a18712d8c78e2
PaulAllan209 <dpaulallan14@gmail.com>
2025-05-06 13:39:42 +0800

    added save state display in the user bar

--- markdown_note_taking_app.client/src/App.jsx
+++ markdown_note_taking_app.client/src/App.jsx
@@ -3,11 +3,21 @@ import SideBar from './SideBar';
 import DisplayWindow from './DisplayWindow';
 import EditingWindow from './EditingWindow';
 import UserWindowBar from './UserWindowBar';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { debounce } from 'lodash';
 
 function App() {
     const [selectedFileGuid, setSelectedFileGuid] = useState(null);
+    const [fileContentInDb, setFileContentInDb] = useState(null);
     const [fileContent, setFileContent] = useState(null);
+    const [isSaved, setIsSaved] = useState(true);
+
+    const debouncedSaveCheck = useCallback(
+        debounce((content, dbContent) => {
+            setIsSaved(content === dbContent);
+        }, 500),
+        []
+    );
 
     useEffect(() => {
         if (selectedFileGuid != null) {
@@ -23,15 +33,22 @@ function App() {
                 })
                 .then(data => {
                     setFileContent(data.fileContent || '');
+                    setFileContentInDb(data.fileContent || '');
                 })
         }
     }, [selectedFileGuid]);
+
+    useEffect(() => {
+        if (fileContent !== null && fileContentInDb !== null) {
+            debouncedSaveCheck(fileContent, fileContentInDb);
+        }
+    }, [fileContent, fileContentInDb, debouncedSaveCheck]);
 
     return (
         <div className="app-container">
             <SideBar onFileSelect={setSelectedFileGuid} />
             <div className="user-window">
-                <UserWindowBar />
+                <UserWindowBar saveState={isSaved} setSaveState={setIsSaved} />
                 <div className="window-content-container">
                     <EditingWindow selectedFileContent={fileContent} setContent={setFileContent} />
                     <DisplayWindow selectedFileContent={fileContent} />
--- markdown_note_taking_app.client/src/UserWindowBar.jsx
+++ markdown_note_taking_app.client/src/UserWindowBar.jsx
@@ -1,12 +1,18 @@
 import './UserWindowBar.css';
 
-function UserWindowBar() {
+function UserWindowBar(props) {
+
+    const handleSave = () => {
+        props.setSaveState(true);
+    }
+
     return (
         <div className="user-bar">
+            <p className="save-state">{ props.saveState ? "Saved" : "Unsaved"}</p>
             <div className="user-bar-buttons-container">
+                <button className="user-bar-buttons" onClick={handleSave}>Save</button>
                 <button className="user-bar-buttons">Export as Markdown</button>
                 <button className="user-bar-buttons">Export as HTML</button>
-                <button className="user-bar-buttons">Save</button>
             </div>
         </div>
   );
