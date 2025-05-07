09b1325f35d5da0b0e6dcae26899de7f5f14af0a
PaulAllan209 <dpaulallan14@gmail.com>
2025-05-07 13:35:39 +0800

    successfully added accept changes window

--- markdown_note_taking_app.client/src/AcceptChangesWindow.jsx
+++ markdown_note_taking_app.client/src/AcceptChangesWindow.jsx
@@ -1,13 +1,61 @@
 import './AcceptChangesWindow.css';
+import React, { useContext } from 'react';
+import { AcceptChangesWindowContext } from './App.jsx';
 
 function AcceptChangesWindow() {
+    const
+        {
+            grammarCheckedFileContent,
+            setGrammarCheckedFileContent,
+            setFileContent,
+            setFileContentInDb,
+            setShowGrammarView,
+            selectedFileGuid,
+            setIsSaved
+        } = useContext(AcceptChangesWindowContext);
+
+    const handleAcceptChanges = (fileId, grammarCheckedFileContent) => {
+        const patchDocument = [
+            {
+                "op": "replace",
+                "path": "/fileContent",
+                "value": grammarCheckedFileContent
+            }
+        ];
+
+        fetch(`https://localhost:7271/api/markdown/${fileId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json-patch+json'
+            },
+            body: JSON.stringify(patchDocument)
+        })
+            .then(response => {
+                if (response.ok) {
+                    console.log("Successfully saved the file to the database.");
+                    setIsSaved(true);
+                    setShowGrammarView(false);
+                    setFileContent(grammarCheckedFileContent);
+                    setFileContentInDb(grammarCheckedFileContent);
+                    setGrammarCheckedFileContent('');
+                }
+                else {
+                    console.error("Failed to save the file");
+                    alert("Failed to save the file");
+                }
+            });
+    };
+
+    const handleRejectChanges = () => {
+        setShowGrammarView(false);
+    }
     return (
        <div className="accept-changes-window">
             <div className="accept-changes-text">
                 Accept Changes?
             </div>
-            <button className="accept-button">&#10003;</button>
-            <button className="reject-button">&#10007;</button>
+            <button className="accept-button" onClick={() => handleAcceptChanges(selectedFileGuid, grammarCheckedFileContent) }>&#10003;</button>
+            <button className="reject-button" onClick={handleRejectChanges}>&#10007;</button>
       </div>
   );
 }
--- markdown_note_taking_app.client/src/App.jsx
+++ markdown_note_taking_app.client/src/App.jsx
@@ -4,8 +4,10 @@ import DisplayWindow from './DisplayWindow';
 import EditingWindow from './EditingWindow';
 import UserWindowBar from './UserWindowBar';
 import GrammarSuggestionWindow from './GrammarSuggestionWindow';
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, createContext, useCallback } from 'react';
 import { debounce } from 'lodash';
+
+export const AcceptChangesWindowContext = createContext();
 
 function App() {
     const [selectedFileGuid, setSelectedFileGuid] = useState(null);
@@ -22,7 +24,7 @@ function App() {
         []
     );
 
-    // Getting the file content
+    // Getting the file content if selected file and file content chnanges
     useEffect(() => {
         if (selectedFileGuid != null) {
             fetch(`https://localhost:7271/api/markdown/${selectedFileGuid}`)
@@ -43,6 +45,7 @@ function App() {
     }, [selectedFileGuid]);
 
     // Saving functionality
+    // PATCH Request api for saving is in UserWindowBar.jsx
     useEffect(() => {
         if (fileContent !== null && fileContentInDb !== null) {
             debouncedSaveCheck(fileContent, fileContentInDb);
@@ -66,20 +69,36 @@ function App() {
                     setGrammarCheckedFileContent(data.fileContent || '');
                 })
         }
+        else {
+            setGrammarCheckedFileContent('');
+        }
     }, [showGrammarView]);
 
     return (
         <div className="app-container">
             <SideBar onFileSelect={setSelectedFileGuid} />
             <div className="user-window">
-                <UserWindowBar
-                    saveState={isSaved}
-                    setSaveState={setIsSaved}
-                    fileGuid={selectedFileGuid}
-                    fileCurrentContent={fileContent}
-                    showGrammarView={showGrammarView}
-                    setShowGrammarView={setShowGrammarView}
-                    setGrammarCheckedFileContent={setGrammarCheckedFileContent} />
+                <AcceptChangesWindowContext.Provider value={
+                    {
+                        grammarCheckedFileContent,
+                        setGrammarCheckedFileContent,
+                        setFileContent,
+                        setFileContentInDb,
+                        setShowGrammarView,
+                        selectedFileGuid,
+                        setIsSaved
+                    }
+                }>
+                    <UserWindowBar
+                        saveState={isSaved}
+                        setSaveState={setIsSaved}
+                        fileGuid={selectedFileGuid}
+                        fileCurrentContent={fileContent}
+                        showGrammarView={showGrammarView}
+                        setShowGrammarView={setShowGrammarView}
+                        setGrammarCheckedFileContent={setGrammarCheckedFileContent} />
+                </AcceptChangesWindowContext.Provider>
+               
                 <div className="window-content-container">
                     {!showGrammarView ? <EditingWindow selectedFileContent={fileContent} setContent={setFileContent} /> :
                         <GrammarSuggestionWindow grammarCheckedFileContent={grammarCheckedFileContent} setGrammarCheckedFileContent={setGrammarCheckedFileContent} />}
