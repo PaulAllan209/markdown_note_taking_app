fc8ef3829697da759f9398d0f6afd3d1c51968fe
PaulAllan209 <dpaulallan14@gmail.com>
2025-05-08 13:53:36 +0800

    implemented handleFileContentSave from apiUtils in AcceptChangesWindow to solve boilerplate code

--- markdown_note_taking_app.client/src/AcceptChangesWindow.jsx
+++ markdown_note_taking_app.client/src/AcceptChangesWindow.jsx
@@ -1,6 +1,7 @@
 import './AcceptChangesWindow.css';
 import React, { useContext } from 'react';
 import { AcceptChangesWindowContext } from './contexts/AcceptChangesWindowContext.jsx';
+import { handleFileContentSave } from './utils/apiUtils.js';
 
 
 function AcceptChangesWindow() {
@@ -16,35 +17,18 @@ function AcceptChangesWindow() {
         } = useContext(AcceptChangesWindowContext);
 
     const handleAcceptChanges = (fileId, grammarCheckedFileContent) => {
-        const patchDocument = [
-            {
-                "op": "replace",
-                "path": "/fileContent",
-                "value": grammarCheckedFileContent
-            }
-        ];
 
-        fetch(`https://localhost:7271/api/markdown/${fileId}`, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json-patch+json'
-            },
-            body: JSON.stringify(patchDocument)
-        })
-            .then(response => {
-                if (response.ok) {
-                    console.log("Successfully saved the file to the database.");
-                    setIsSaved(true);
-                    setShowGrammarView(false);
-                    setFileContent(grammarCheckedFileContent);
-                    setFileContentInDb(grammarCheckedFileContent);
-                    setGrammarCheckedFileContent('');
-                }
-                else {
-                    console.error("Failed to save the file");
-                    alert("Failed to save the file");
-                }
-            });
+        handleFileContentSave(
+            fileId,
+            grammarCheckedFileContent,
+            //onSuccess callback
+            () => {
+                setIsSaved(true);
+                setShowGrammarView(false);
+                setFileContent(grammarCheckedFileContent);
+                setFileContentInDb(grammarCheckedFileContent);
+                setGrammarCheckedFileContent('');
+            })
     };
 
     const handleRejectChanges = () => {
